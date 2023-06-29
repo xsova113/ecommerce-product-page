5145cb107245bb36706b79f092b4ecb7f13e9b92
George <xsova113@gmail.com>
2023-06-29 14:34:44 -0700

    minor update

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -3,8 +3,8 @@ import type { Image } from "sanity";
 // import { dataset, projectId } from '../env'
 
 const imageBuilder = createImageUrlBuilder({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "gotzyoih",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "",
 });
 
 export const urlFor = (source: Image) => {
